9e92594f0174887f332dfb71d787bbd86b28125f
Dhanvant <92245578+Dhanvantg@users.noreply.github.com>
2023-08-09 23:25:34 +0530

    gui integration

--- detect.py
+++ detect.py
@@ -1,53 +1,22 @@
-import cv2
-import numpy as np
-from PIL import Image
 import imutils
-from tensorflow.keras.models import load_model
-import time
-import keras_ocr
+import cv2
 import matplotlib.pyplot as plt
-
-
+import numpy as np
+import keras_ocr
 #pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 #os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'false'
 
-net = cv2.dnn.readNet("yolov3-custom_7000.weights", "yolov3-custom.cfg")
-net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
-
-pipeline = keras_ocr.pipeline.Pipeline()
-
-model = load_model('helmet-nonhelmet_cnn.h5')
-print('model loaded!!!')
-cap = cv2.VideoCapture(r"bs.mp4")
+#cap = cv2.VideoCapture(r"asmall.mp4")
 COLORS = [(0, 255, 0), (0, 0, 255)]
 
-layer_names = net.getLayerNames()
-output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
-
-fourcc = cv2.VideoWriter_fourcc(*"XVID")
-writer = cv2.VideoWriter('output.avi', fourcc, 5, (888, 500))
+#fourcc = cv2.VideoWriter_fourcc(*"XVID")
+#writer = cv2.VideoWriter('output.avi', fourcc, 5, (888, 500))
 
 
-def helmet_or_nohelmet(helmet_roi):
-    try:
-        helmet_roi = cv2.resize(helmet_roi, (224, 224))
-        helmet_roi = np.array(helmet_roi, dtype='float32')
-        helmet_roi = helmet_roi.reshape(1, 224, 224, 3)
-        helmet_roi = helmet_roi / 255.0
-        return int(model.predict(helmet_roi)[0][0])
-    except:
-        pass
-
-
-ret = True
-
-while True:
-
-    ret, img = cap.read()
+def detect_helmet(img, n, gap, model, pipeline, output_layers, net):
     if img is None:
         print('EMPTY')
-        continue
+        return
     #cv2.imshow('TEST', img)
     #time.sleep(5)
     img = imutils.resize(img, height=500)
@@ -85,57 +54,72 @@ while True:
 
     for i in range(len(boxes)):
         if i in indexes:
-            print(boxes[i])
+            #print(boxes[i])
             x, y, w, h = boxes[i]
             color = [int(c) for c in COLORS[classIds[i]]]
             # green --> bike
             # red --> number plate
             if classIds[i] == 0:  # bike
-                helmet_roi = img[max(0, y):max(0, y) + max(0, h) // 4, max(0, x):max(0, x) + max(0, w)]
-            else:  # number plate
-                x_h = x - 60
-                y_h = y - 350
-                w_h = w + 100
-                h_h = h + 100
+                pass
+                # helmet_roi = img[max(0, y):max(0, y) + max(0, h) // 4, max(0, x):max(0, x) + max(0, w)]
+            else:  # Helmet Detection
+                x_h = x
+                y_h = y - 400
+                w_h = w + 300
+                h_h = h + 300
                 cv2.rectangle(img, (x, y), (x + w, y + h), color, 7)
                 # h_r = img[max(0,(y-330)):max(0,(y-330 + h+100)) , max(0,(x-80)):max(0,(x-80 + w+130))]
                 plate = img[y:y + h, x:x + w]
-                cv2.imshow('Number Plate', plate)
-                #cv2.imshow('BOZ', h_n)
+                cv2.imwrite('platemp.png', plate)
                 if y_h > 0 and x_h > 0:
-                    h_r = img[y_h:y_h + h_h, x_h:x_h + w_h]
-                    c = helmet_or_nohelmet(h_r)
-                    #if c == 0:
-                    cv2.imwrite('1.png', plate)
-                    images = [keras_ocr.tools.read('1.png')]
-                    plt.figure(figsize=(10, 20))
-                    fig, axs = plt.subplots(nrows=1, figsize=(10, 20), squeeze=False)
-                    #print(axs)
-                    prediction_groups = pipeline.recognize(images)
-                    
-                    #axs = axs.flatten()
-                    #for ax, image, predictions in zip(axs, images, prediction_groups):
-                        #an = keras_ocr.tools.drawAnnotations(image=image,
-                                                        #predictions=predictions,
-                                                        #ax=ax)
-                        #plt.show()
-
+                    h_r1 = img[y_h:y_h + h_h, x_h:x_h + w_h]
+                    #cv2.imshow('helmet', h_r)
+                    # Helmet Detection
+                    try:
+                        h_r = cv2.resize(h_r1, (224, 224))
+                        h_r = np.array(h_r, dtype='float32')
+                        h_r = h_r.reshape(1, 224, 224, 3)
+                        h_r = h_r / 255.0
+                        c = int(model.predict(h_r)[0][0])
+                    except:
+                        c = None
+                    if c == 0:
+                        print("NO HELMET!")
+                        images = [keras_ocr.tools.read('platemp.png')]
+                        prediction_groups = pipeline.recognize(images)
+                        print(prediction_groups)
+                        cv2.imwrite('plate'+str(n)+'.png', plate)
+                        cv2.imwrite('face' + str(n) + '.png', h_r1)
+                        text = ''
+                        for i in prediction_groups[0]:
+                            text += i[0]
+                        gap = False
+                        cv2.imwrite('temp.png', img)
+                        return 'face' + str(n) + '.png', 'plate'+str(n)+'.png', text, 'temp.png', n+1, gap
+                    else:
+                        gap = True
                     cv2.putText(img, ['helmet', 'no-helmet'][c], (x, y - 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0),
                                 2)
                     cv2.rectangle(img, (x_h, y_h), (x_h + w_h, y_h + h_h), (255, 0, 0), 10)
+                cv2.imwrite('temp.png', img)
+                return 'temp.png', n+1, gap
+    cv2.imwrite('temp.png', img)
 
-    writer.write(img)
-    try:
-        cv2.imshow("Image", img)
-    except:
-        pass
 
-    if cv2.waitKey(1) == 27:
-        break
+    #writer.write(img)
+    #try:
+        #cv2.imshow("Image", img)
+    #except:
+        #pass
 
-writer.release()
-cap.release()
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    #if cv2.waitKey(1) == 27:
+        #break
+
+#writer.release()
+#cap.release()
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
 # https://machinelearningprojects.net/helmet-and-number-plate-detection-and-recognition/
 # https://drive.google.com/file/d/1o3i0VhhTjImEAl260M3EoMU5WNpRM-s1/view?usp=sharing
+# https://drive.google.com/file/d/1NztDuFsjdHgkMskSKn4Dj_O2jEGKnllo/view?usp=sharing
+# https://drive.google.com/file/d/1RySiJvPIRsA-fio9_ViGzS8OZg1eymPg/view?usp=sharing
--- gui.py
+++ gui.py
@@ -1,16 +1,33 @@
 from PIL import Image, ImageTk
-
-
-from tkinter import Tk, Label, Canvas, NW, Button, PhotoImage,ttk, Text, Frame
-from tkinter import filedialog
+from tkinter import Tk, Label, Canvas, NW, Button, PhotoImage,ttk, Text, Frame, filedialog
 import cv2
-
+import keras_ocr
+from tensorflow.keras.models import load_model
+import numpy as np
+import imutils
+import matplotlib.pyplot as plt
+from detect import detect_helmet
+from threading import *
 
 root = Tk()
 root.title("HELMET DETECTOR")
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
 (root.geometry("%dx%d" % (w, h)))
 
+images = ["nil.png"]  # , "image2.png", "image3.png"]
+images_new = ["nilp.png"]  # , "img1.png", "img2.png"]
+text_new = [""]  # ,"ABCD012","DL4C AF 4943"]
+
+net = cv2.dnn.readNet("yolov3-custom_7000.weights", "yolov3-custom.cfg")
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+
+pipeline = keras_ocr.pipeline.Pipeline()
+
+model = load_model('helmet-nonhelmet_cnn.h5')
+print('model loaded!!!')
 
 class VideoPlayer:
     def __init__(self, root, video_path,initial_x,initial_y):
@@ -44,7 +61,7 @@ def next():
     global current_image_index
     global image_label
     global img_label
-
+    print(images_new)
     current_image_index = (current_image_index + 1) % len(images)
     image = PhotoImage(file=images[current_image_index])
     image_label.configure(image=image)
@@ -74,17 +91,54 @@ def back():
     text_box.delete("1.0", "end")
     text_box.insert("1.0", text_new[current_image_index])
 
-
 if __name__ == "__main__":
-
-
     video_path = "video.mp4"
     initial_x = 0
-    initial_y = h//4
+    initial_y = 0
+    cap = cv2.VideoCapture('asmall.mp4')
+    #player = VideoPlayer(root, video_path, initial_x, initial_y)
+    n = 1
+    gap = True
+    def update():
+        global n, gap
+        #global image, image_label
+        imag = PhotoImage(file='load.png')
+        imag_label = Label(root, image=imag)
+        imag_label.place(x=0, y=0)
+        while True:
+            ret, fr = cap.read()
+            detection = detect_helmet(fr, n, gap, model, pipeline, output_layers, net)
+            if detection is None:
+                print('none')
+                continue
+            elif len(detection) == 3:
+                img, n, gap = detection
+            elif len(detection) == 6:
+                face, plate, text, img, n, gap = detection
+                images.append(face)
+                images_new.append(plate)
+                text_new.append(text)
+                head = Image.open(face)
+                head = head.resize((w // 2, h // 4))
+                head.save(face)
+                num = Image.open(plate)
+                num = num.resize((w // 2, h // 4))
+                num.save(plate)
 
-    player = VideoPlayer(root, video_path, initial_x, initial_y)
+            else:
+                print('what')
+                continue
 
-    images = ["image1.png", "image2.png", "image3.png"]
+            frm = Image.open(img)
+            frm = frm.resize((w // 2, h // 2))
+            frm.save(img)
+            imag = PhotoImage(file=img)
+            imag_label.configure(image=imag)
+            imag_label.image = imag
+            #_label = Label(root, image=frm)
+            #frm_label.place(x=0, y=0)
+        #root.after(50, update)
+
     for i in images:
         plate = Image.open(i)
         plate = plate.resize((w // 2, h // 4))
@@ -94,22 +148,20 @@ if __name__ == "__main__":
     image_label = Label(root, image=image)
     image_label.place(x=w//2, y=0)
 
-
-    images_new = ["image.png", "img1.png", "img2.png"]
     for i in images_new:
         plate1 = Image.open(i)
         plate1 = plate1.resize((w // 2, h // 4))
         plate1.save(i)
     img = PhotoImage(file=images_new[current_image_index])
     border_color = Frame(root, background="red")
-    img_label = Label(root, image=img,border_color,)
+    img_label = Label(root, image=img)
     img_label.place(x=w//2, y=h//4)
 
 
     button = Button(root, text="back",command=back)
-    button.place(x=w*3//4 - 100, y=h*3//4+50, width=100, height=30)
+    button.place(x=w//2 - 150, y=h*3//4+50, width=100, height=30)
     button1 = Button(root, text="next", command=next)
-    button1.place(x=w*3//4 + 100, y=h*3//4+50, width=100, height=30)
+    button1.place(x=w//2 + 50, y=h*3//4+50, width=100, height=30)
 
     '''plate = Image.open('image.png')
     plate = plate.resize((w // 2, h // 4))
@@ -119,12 +171,14 @@ if __name__ == "__main__":
     image_lab.place(x=w//2, y=0)'''
 
 
-    text_box = Text(root, height=5, width=40, bg="light cyan")
-    text_new= ["KL01 BR 8055","ABCD012","DL4C AF 4943"]
+    text_box = Text(root, height=5, width=40, bg="light grey")
 
     text_box.insert("1.0", text_new[current_image_index])
-    text_box.place(x=w*5//8+100, y=h*5//8)
+    text_box.place(x=w//2-150, y=h*5//8)
 
+    t1 = Thread(target=update)
+    t1.start()
     root.mainloop()
+    print('hi')
 
 
